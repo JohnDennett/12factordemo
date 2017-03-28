4a3a1faca5c2df8635d7aba49d0965248f50519c
John Dennett <john.dennett@capgemini.com>
2017-03-28 10:12:59 -0400

    small comment in source

--- src/main/java/dk/mwl/training/Application.java
+++ src/main/java/dk/mwl/training/Application.java
@@ -1,12 +1,28 @@
 package dk.mwl.training;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
-	public static void main(String[] args) {
+    @GetMapping
+    public String root(@Value("${greeter.name}") String greeterName) {
+        return "Hello " + greeterName;
+    }
+// this is a change by John Dennett
+    @Bean
+    public CommandLineRunner cliRunner(@Value("${greeter.name}") String greeterName) {
+        return args -> System.out.println("Hello " + greeterName);
+    }
+
+    public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
